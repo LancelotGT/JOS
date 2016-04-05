3c242c87788498c3bc4e454516ba3ee71114c552
David Benas <dcbenas@gmail.com>
2016-04-05 15:09:53 -0400

    BAR 0 Mapping completed.

--- kern/e1000.c
+++ kern/e1000.c
@@ -4,5 +4,7 @@
 int e1000_attach(struct pci_func *pcif)
 {
     pci_func_enable(pcif);
+    e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]); // VM mapping for BAR 0
+    cprintf("Status is: 0x%x. Desired: 0x80080783\n", e1000[E1000_STATUS_REG]);
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,9 +1,13 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include <kern/pmap.h>
 #include <kern/pci.h>
 
+#define E1000_STATUS_REG 0x00008/4 // 4 byte register starting at 8th byte of reg space
+
 int e1000_attach(struct pci_func *pcif); 
+volatile uint32_t *e1000;
 
 #endif	// JOS_KERN_E1000_H
 
