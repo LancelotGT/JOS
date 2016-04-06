25ba95d627f41545fd65b8d0434eb6454e9ec002
Ning Wang <glningwang@gmail.com>
2016-04-06 11:47:12 -0400

    init transmission

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,10 +1,25 @@
 #include <kern/e1000.h>
 
+static struct tx_desc tx_descs[NTDESC];
+
 // LAB 6: Your driver code here
 int e1000_attach(struct pci_func *pcif)
 {
     pci_func_enable(pcif);
     e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]); // VM mapping for BAR 0
-    cprintf("Status is: 0x%x. Desired: 0x80080783\n", e1000[E1000_STATUS_REG]);
+    cprintf("Status is: 0x%x. Desired: 0x80080783\n", e1000[E1000_STATUS]);
+
+    assert(sizeof(tx_descs) % 128 == 0); // should be 128-byte aligned
+
+    // perform transmit initialization
+    e1000[E1000_TDBAL] = PADDR(tx_descs);
+    e1000[E1000_TDLEN] = sizeof(tx_descs);
+    e1000[E1000_TDH] = 0;
+    e1000[E1000_TDT] = 0;
+    e1000[E1000_TCTL] |= E1000_TCTL_EN;
+    e1000[E1000_TCTL] |= E1000_TCTL_PSP;
+    e1000[E1000_TCTL] |= E1000_TCTL_CT_INIT;
+    e1000[E1000_TCTL] |= E1000_TCTL_COLD_INIT;
+    e1000[E1000_TIPG] |= E1000_TIPG_INIT;
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -4,10 +4,46 @@
 #include <kern/pmap.h>
 #include <kern/pci.h>
 
-#define E1000_STATUS_REG 0x00008/4 // 4 byte register starting at 8th byte of reg space
+#define E1000_STATUS 0x00008 / 4 // 4 byte register starting at 8th byte of reg space
+#define E1000_TDBAL 0x03800 / 4  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDLEN    0x03808 / 4  /* TX Descriptor Length - RW */ 
+#define E1000_TDH      0x03810 / 4  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818 / 4  /* TX Descripotr Tail - RW */ 
+#define E1000_TCTL     0x00400 / 4  /* TX Control - RW */
+#define E1000_TIPG     0x00410 / 4  /* Transmit IPG Register */
+
+/* Transmit Control Registers */
+#define E1000_TCTL_RST    0x00000001    /* software reset */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_BCE    0x00000004    /* busy check enable */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+#define E1000_TCTL_SWXOFF 0x00400000    /* SW Xoff transmission */
+#define E1000_TCTL_PBE    0x00800000    /* Packet Burst Enable */
+#define E1000_TCTL_RTLC   0x01000000    /* Re-transmit on late collision */
+#define E1000_TCTL_NRTU   0x02000000    /* No Re-transmit on underrun */
+#define E1000_TCTL_MULR   0x10000000    /* Multiple request support */ 
+
+#define E1000_TCTL_CT_INIT 0x00000010    /* initial collision threshold */  
+#define E1000_TCTL_COLD_INIT 0x00040000    /* initial collision distance */  
+#define E1000_TIPG_INIT 0x0060200a /* init values for TIPG in 13.4.34 */
+
+#define NTDESC 32 // the number of the transmit descriptors, somehow arbitrary
 
 int e1000_attach(struct pci_func *pcif); 
 volatile uint32_t *e1000;
+
+struct tx_desc
+{
+    uint64_t addr;
+    uint16_t length;
+    uint8_t cso;
+    uint8_t cmd;
+    uint8_t status;
+    uint8_t css;
+    uint16_t special;
+} __attribute__ ((packed));
 
 #endif	// JOS_KERN_E1000_H
 
